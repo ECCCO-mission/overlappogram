dd1ae03952683af2c6ad4c9aee81454bf0bf4514
Marcus Hughes <hughes.jmb@gmail.com>
2024-02-26 12:40:45 -0600

    adds config based execution

---
+++ run_multiion_inversion.py
@@ -0,0 +1,60 @@
+import argparse
+import os
+import time
+
+import toml
+from sklearn.linear_model import ElasticNet
+
+from overlappogram.elasticnet_model import ElasticNetModel as model
+from overlappogram.inversion_field_angles import Inversion
+
+
+def run_inversion(config: dict):
+    inversion = Inversion(rsp_func_cube_file=config['paths']['response'],
+                          rsp_dep_name=config['settings']['response_dependency_name'],
+                          rsp_dep_list=config['settings']['response_dependency_list'],
+                          solution_fov_width=config['settings']['solution_fov_width'],
+                          smooth_over=config['settings']['smooth_over'],
+                          field_angle_range=config['settings']['field_angle_range'])
+
+    inversion.initialize_input_data(config['paths']['image'],
+                                    None,
+                                    config['paths']['weights'])
+
+    for alpha in config['settings']['alphas']:
+        for rho in config['settings']['rhos']:
+            enet_model = ElasticNet(alpha=alpha,
+                                    l1_ratio=rho,
+                                    max_iter=100000,
+                                    precompute=True,
+                                    positive=True,
+                                    fit_intercept=False,
+                                    selection='cyclic')
+            inv_model = model(enet_model)
+            basename = os.path.splitext(os.path.basename(config['paths']['image']))[0]
+
+            start = time.time()
+
+            postfix = 'x'+str(config['settings']['solution_fov_width'])+'_'+str(rho*10)+'_'+str(alpha)+'_wpsf'
+            inversion.multiprocessing_invert(inv_model,
+                                             config['paths']['output'],
+                                             output_file_prefix=basename,
+                                             output_file_postfix=postfix,
+                                             detector_row_range=config['settings']['detector_row_range'],
+                                             score=False)
+
+            end = time.time()
+            print("Inversion Time =", end - start)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Inverts overlappograms')
+
+    parser.add_argument('config')
+    args = parser.parse_args()
+
+    # read config
+    with open(args.config) as f:
+        config = toml.load(f)
+
+    run_inversion(config)

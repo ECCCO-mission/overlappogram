e39cd13a9f3ea0a304be7eda5fd4daa52984b1e6
Marcus Hughes <hughes.jmb@gmail.com>
2024-06-06 11:14:56 -0400

    create invalid data error

--- overlappogram/error.py
+++ overlappogram/error.py
@@ -4,3 +4,11 @@ class OverlappogramWarning(Warning):
 
 class NoWeightsWarnings(OverlappogramWarning):
     """There are no weights passed to unfold."""
+
+
+class OverlappogramError(Exception):
+    pass
+
+
+class InvalidDataFormatError(OverlappogramError):
+    """The data file has an error that prevents processing."""
